322ae7fbe9e01557f21cf593697d052fd3a303e4
SimonFolger <simon.folger@gmx.de>
2018-06-07 13:39:08 +0200

    Update AfterPrice.php
    make message visible on category page

--- Block/AfterPrice.php
+++ Block/AfterPrice.php
@@ -127,7 +127,7 @@ class AfterPrice extends \Magento\Framework\View\Element\Template
         }
 
         if ($taxRate <= 0 || !$product) {
-            return '';
+            $taxRate = '';
         } else {
             $taxRate .= '%';
         }
